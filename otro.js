b3adbcf62bef8fc923d7186cc9a4c5005dea496c
juanmi-devv <tabletsanchez3@gmail.com>
2025-12-12 01:39:42 +0100

    01-Práctica finalizada JS

--- otro.js
+++ otro.js
@@ -1,4 +1,4 @@
-const nombre = "Tu nombre";
+const nombre = "Juanillo";
 let edad = 20;
 let estudiante = true;
 let hobbies = ["leer", "jugar", "viajar"];
@@ -18,7 +18,35 @@ const persona = {
 
 //console.log(persona);
 
-// Acceso a datos.
+// Acceso a datos y mostrar su tipo.
 
-console.log(persona.nombre);
+console.log("nombre:", persona.nombre, typeof persona.nombre);
+console.log("edad:", persona.edad, typeof persona.edad);
+console.log("estudiante:", persona.estudiante, typeof persona.estudiante);
+console.log("mascota:", persona.mascota, typeof persona.mascota); // En JS null devuelve 'object'
+console.log("curso:", persona.curso, typeof persona.curso); // undefined
 
+
+
+
+// Acceso especial al array de hobbies
+console.log("hobbies (completo):", persona.hobbies, typeof persona.hobbies);
+console.log("Hobby 1:", persona.hobbies[0]); // Acceso por índice 0
+console.log("Hobby 2:", persona.hobbies[1]); // Acceso por índice 1
+console.log("Hobby 3:", persona.hobbies[2]); // Acceso por índice 2
+
+
+// Agregar datos a un objeto.
+
+persona.pais = "España";
+persona.peso = 70;
+
+//console.log("Objeto persona actualizado:", persona);
+
+
+// Eliminar datos.
+
+delete persona.estudiante;
+delete persona.curso;
+
+console.log("Objeto persona después de eliminar propiedades:", persona);
