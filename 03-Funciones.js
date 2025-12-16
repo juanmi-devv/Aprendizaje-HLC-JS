25f1276ec1b5e1705c962c30d3804f7f60971220
juanmi-devv <tabletsanchez3@gmail.com>
2025-12-16 13:16:30 +0100

    funciones

---
+++ 03-Funciones.js
@@ -0,0 +1,21 @@
+// Funcion (en esta funcion con array fuction .push 
+
+function tablaDeMultiplicar(valor) {
+    let datos = []
+    for(let numero = 0; numero < 10; numero++) {
+        datos.push(valor + " X " + numero + "=" + valor*numero)
+        alert(tablaDeMultiplicar)
+    }
+    return datos
+}
+
+// Funcion facil.
+/*
+function sumar(num1, num2) {
+    let num3 = 10
+    let resultado = num1 + num2 + num3
+    return resultado
+}
+
+alert(sumar(1,3))
+*/
