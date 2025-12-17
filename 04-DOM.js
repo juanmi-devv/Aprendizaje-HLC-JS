cd2db3961d1d93d35e31d61e052d8767784aae85
juanmi-devv <tabletsanchez3@gmail.com>
2025-12-17 14:49:52 +0100

    Hecho

---
+++ 04-DOM.js
@@ -0,0 +1,5 @@
+// DOM.
+
+let caja = document.getElementById('miCaja');
+caja.style.color = 'red'; // Cambia el color del texto a azul
+caja.style.fontSize = '254px'; // Cambia el tamaño de la fuente
